c9788386ed56a4d83c5a09ba604da98ede53de00
rafaeldenis <rafagdf85@gmail.com>
2021-03-11 23:15:41 -0300

    refresh-Deslogar

--- deslogar.php
+++ deslogar.php
@@ -4,7 +4,14 @@ include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
 
-session_start();
+//session_start();
+
+$URL_BASE = "http://$_SERVER[HTTP_HOST]";
+     if($URL_BASE=="http://localhost"){
+		$diretorioProjeto = "/php-crud";
+		$URL_BASE = $URL_BASE.$diretorioProjeto;
+	 }
+
 
 $_SESSION['permissao'] = "NAO";
 unset($_SESSION['msgErroExpirou']);
